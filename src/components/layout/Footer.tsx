fdec9a91d4b24cf3e9ba503343bfd982bd57ff53
Anuj-Kaundal <kaundalanuj45@gmail.com>
2026-08-29 10:41:05 +0530

    add new social media icons in footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -16,6 +16,21 @@ import { BRAND_INFO } from '../../data/products';
 
 const logoLight = '/images/logo-light.png';
 
+const TikTokIcon: React.FC<{ className?: string }> = ({ className }) => (
+  <svg
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    className={className}
+    aria-hidden="true"
+  >
+    <path
+      d="M14.5 3c.6 2 2.1 3.3 4.3 3.5v2.6c-1.3 0-2.5-.3-3.6-.9v6.2c0 3.2-2.6 5.8-5.8 5.8s-5.8-2.6-5.8-5.8 2.6-5.8 5.8-5.8c.4 0 .7 0 1 .1v2.8c-.3-.1-.6-.1-1-.1-1.7 0-3.1 1.4-3.1 3.1s1.4 3.1 3.1 3.1 3.1-1.4 3.1-3.1V3h3.9Z"
+      fill="currentColor"
+    />
+  </svg>
+);
+
 export const Footer: React.FC = () => {
   const { navigateToPage, setIsWelcomePopupOpen } = useShop();
 
@@ -263,7 +278,30 @@ export const Footer: React.FC = () => {
                 <Facebook className="w-4 h-4" />
               </a>
 
-              <button
+              <a
+                href="https://tiktok.com"
+                target="_blank"
+                rel="noreferrer"
+                className="
+                  w-9 h-9
+                  border
+                  flex
+                  items-center
+                  justify-center
+                  transition-all
+                  shadow-md
+                  border-[#D4AF37]/30
+                  bg-[#0C8A9B]
+                  text-[#D4AF37]
+                  hover:bg-[#D4AF37]
+                  hover:text-[#062B35]
+                "
+                aria-label="TikTok"
+              >
+                <TikTokIcon className="w-4 h-4" />
+              </a>
+
+              {/* <button
                 onClick={() => navigateToPage('contact')}
                 className="
                   w-9 h-9
@@ -284,7 +322,7 @@ export const Footer: React.FC = () => {
                 title="Global Maison Concierge"
               >
                 <Globe className="w-4 h-4" />
-              </button>
+              </button> */}
 
             </div>
           </div>
